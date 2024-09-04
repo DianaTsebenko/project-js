8093ecdebc50b945621a3bdc1ac9fb452b24da95
MrScreamer <nostrees@ukr.net>
2024-09-04 03:03:56 +0200

    fully completed with all fixes

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const refs = {
     modal: document.getElementById('event-modal'),
     list: document.getElementById('events-list'),
-    modalContent: document.querySelector('.modal__content'),
+    modalContent: document.querySelector('.modal__window'),
   };
 
  // Handle opening the modal
@@ -34,8 +34,17 @@ function onOpenModal(evt) {
     url
   } = eventData;
 
-  refs.modalContent.innerHTML = `
+  
 
+  refs.modalContent.innerHTML = `
+<div class="modal__close-btn-container">
+    <div class="modal__close-btn">
+<svg  viewBox="0 0 25 25"  xmlns="http://www.w3.org/2000/svg">
+<path d="M1.28557 25C0.95625 25 0.626931 24.8747 0.376724 24.6227C-0.125574 24.1204 -0.125574 23.306 0.376724 22.8038L22.8043 0.376714C23.3066 -0.125571 24.121 -0.125571 24.6233 0.376714C25.1256 0.878999 25.1256 1.69333 24.6233 2.19593L2.19598 24.6227C1.94389 24.8732 1.61457 25 1.28557 25Z" />
+<path d="M23.7147 25C23.3854 25 23.0564 24.8747 22.8059 24.6227L0.376724 2.19593C-0.125574 1.69333 -0.125574 0.878999 0.376724 0.376714C0.879022 -0.125571 1.69337 -0.125571 2.19598 0.376714L24.6233 22.8038C25.1256 23.306 25.1256 24.1204 24.6233 24.6227C24.3712 24.8732 24.0422 25 23.7147 25Z" />
+</svg>
+    </div>
+</div>
 
     <div class="modal__icon">
       <img src="${images[4]?.url || 'default-image.jpg'}" alt="${name}">
@@ -67,7 +76,7 @@ function onOpenModal(evt) {
         </li>
         <li class="main-info-item">
           <h3>PRICES</h3>
-          <p>
+          <div>
             ${priceRanges[0] ? `
               <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="29" height="20" viewBox="0 0 44 32">
                 <path d="M4.91 1.27h-4.91v29.46h4.91v-29.46z"></path>
@@ -80,6 +89,8 @@ function onOpenModal(evt) {
               </svg>
               Standard ${priceRanges[0]?.min || 'N/A'}-${priceRanges[0]?.max || 'N/A'} ${priceRanges[0]?.currency || 'N/A'}
             ` : 'Price information not available'}
+                      </div>
+              <div>
             ${priceRanges[1] ? `
               <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="29" height="20" viewBox="0 0 44 32">
                 <path d="M4.91 1.27h-4.91v29.46h4.91v-29.46z"></path>
@@ -90,10 +101,10 @@ function onOpenModal(evt) {
                 <path d="M29.46 1.27h-2.381v29.46h2.381v-29.46z"></path>
                 <path d="M34.37 1.27h-2.381v29.46h2.381v-29.46z"></path>
               </svg>
-              VIP ${priceRanges[1]?.min || 'N/A'}-${priceRanges[1]?.max || 'N/A'} ${priceRanges[1]?.currency || 'N/A'}
+              VIP ${priceRanges[1]?.min}-${priceRanges[1]?.max} ${priceRanges[1]?.currency}
             ` : ''}
             <a class="buyTicketsBtn" href="${url || '#'}" target="_blank">BUY TICKETS</a>
-          </p>
+          </div>
         </li>
       </ul>
     </div>
